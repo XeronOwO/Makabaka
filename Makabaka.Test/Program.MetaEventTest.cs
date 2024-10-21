acbaa77ac1d33ec613c43a27695af9f3df9dd6b3
Xeron <2398003522@qq.com>
2024-10-21 19:05:18 +0800

    [opt] 优化心跳事件显示

--- Makabaka.Test/Program.MetaEventTest.cs
+++ Makabaka.Test/Program.MetaEventTest.cs
@@ -5,9 +5,15 @@ namespace Makabaka.Test
 {
 	internal partial class Program
 	{
+		private static bool _firstHeartbeat = true;
+
 		private static Task OnHeartbeat(object sender, HeartbeatEventArgs e)
 		{
-			_logger.LogInformation(nameof(OnHeartbeat));
+			if (_firstHeartbeat)
+			{
+				_firstHeartbeat = false;
+				_logger.LogInformation(nameof(OnHeartbeat));
+			}
 			return Task.CompletedTask;
 		}
 
