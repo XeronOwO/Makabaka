9334fcfc8896b0cc8138db2a177023683b775d99
Xeron <2398003522@qq.com>
2024-10-22 20:45:15 +0800

    [add] 添加获取公告图片链接功能

--- Makabaka/Models/GroupNoticeImageInfo.cs
+++ Makabaka/Models/GroupNoticeImageInfo.cs
@@ -19,5 +19,21 @@
 		/// 图片宽度
 		/// </summary>
 		public string Width { get; set; } = string.Empty;
+
+		/// <summary>
+		/// 获取图片 URL（第一种）
+		/// </summary>
+		/// <param name="scale">图片尺寸</param>
+		/// <returns>图片 URL</returns>
+		public string GetUrl1(int scale = 0)
+			=> $"https://gdynamic.qpic.cn/gdynamic/{Id}/{scale}/";
+
+		/// <summary>
+		/// 获取图片 URL（第二种）
+		/// </summary>
+		/// <param name="scale">图片尺寸</param>
+		/// <returns>图片 URL</returns>
+		public string GetUrl2(int scale = 0)
+			=> $"https://p.qlogo.cn/gdynamic/{Id}/{scale}/";
 	}
 }
